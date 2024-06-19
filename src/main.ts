92aca0aa0789b59ad0baa10661f6ec949c947517
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-19 10:46:41 +0200

    "configuracion Mapbox"

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZnJhbmxpZWJhbmFzIiwiYSI6ImNseDRsOWc3eDBmYzIyanF0dWp1ZDc5dWkifQ.vfbIIRmbb27AX1e7cB31gw';
 
 if( !navigator.geolocation){
   alert('Navegador no soporta el Geolocation');
